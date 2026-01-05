1b1879d184bd29dbc526b756d10a999b11502b70
backfireIBGM <thebeast4028@gmail.com>
2026-01-05 15:20:18 -0800

    Fect: accountPage css update

--- AccountPage.js
+++ AccountPage.js
@@ -68,7 +68,7 @@ function startMissionCountdown(missionId, targetTimeString) {
     if (hoursElem) hoursElem.innerHTML = String(hours).padStart(2, '0');
     if (minutesElem) minutesElem.innerHTML = String(minutes).padStart(2, '0');
     if (secondsElem) secondsElem.innerHTML = String(seconds).padStart(2, '0');
-  }, 1000);
+  });
 }
 
 // Function to load user missions
